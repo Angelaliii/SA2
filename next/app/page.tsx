8b0d433f54db59594f69adbc1d3ee44193a71344
Hongjin <a0908169255@gmail.com>
2025-03-13 15:32:23 +0800

    123

---
+++ next/app/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (<div className={styles.page}><h1>Hello</h1><h2>我是吳濟聰</h2></div>)
+}
