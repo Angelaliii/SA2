eef1f458dd6b361a20b7f6dfcaa345795fedb8ff
Hongjin <a0908169255@gmail.com>
2025-05-13 13:48:32 +0800

    個人頁面刷新問題

--- next/src/app/public-profile/[id]/page.tsx
+++ next/src/app/public-profile/[id]/page.tsx
@@ -397,7 +397,7 @@ export default function PublicProfilePage() {
             >
               <Typography
                 variant="h6"
-                component="a"
+                component="span" // Changed from "a" to "span" to avoid nesting <a> tags
                 sx={{
                   textDecoration: "none",
                   color: "primary.main",
@@ -461,7 +461,7 @@ export default function PublicProfilePage() {
             <Link href={`/Activities/${activity.id}`} passHref>
               <Typography
                 variant="h6"
-                component="a"
+                component="span" // Changed from "a" to "span" to avoid nesting <a> tags
                 sx={{
                   textDecoration: "none",
                   color: "primary.main",
